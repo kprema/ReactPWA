e5fbcf55e92dfeb8df21d832b2c89079c1d5e68e
kprema <kprema301@gmail.com>
2021-08-13 14:08:07 +0530

    Intial commit

---
+++ docs/precache-manifest.839e03524d5a7132c1140002077c0396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec894635c298cc46d4b41cedc745c6c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ae3f39c4968b1e78af8",
+    "url": "./static/css/main.2063f148.chunk.css"
+  },
+  {
+    "revision": "900ae0587202357fe967",
+    "url": "./static/js/2.50d36664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.50d36664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae3f39c4968b1e78af8",
+    "url": "./static/js/main.a7383f06.chunk.js"
+  },
+  {
+    "revision": "5f566252c872a6d458a8",
+    "url": "./static/js/runtime-main.430fa05f.js"
+  }
+]);
